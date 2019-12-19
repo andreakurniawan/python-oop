7e0dd777f46249eecd890d5466942b94d7408077
Andrea Kurniawan <andreakurniawan98@gmail.com>
2019-12-19 22:28:09 +0700

    Materi 5, Belajar implementasi method

---
+++ latihanOOP.py
@@ -0,0 +1,29 @@
+class Hero:
+
+    # Contruct Method
+    def __init__(self, name, health, attackPower, defense):
+        self.name = name
+        self.health = health
+        self.power = attackPower
+        self.defense = defense
+
+    # Fungsi untuk menyerang
+    def serang(self, lawan, nilaiSerang):
+        print(self.name, "Menyerang", lawan.name)
+        # Jika fungsi serang diapnggil, otomatis fungsi diserang() juga terpanggil
+        lawan.diserang(self, self.power)
+
+    # Fungsi untuk diserang
+    def diserang(self, lawan, sisaNyawa):
+        print(self.name, "Diserang", lawan.name)
+        # Mengurangi nilai health dengan nilai serangan lawan
+        effectSerang = self.health - sisaNyawa
+        print("Nyawa terisa : ", effectSerang)
+
+        
+
+
+luffy = Hero("Luffy", 100, 40, 30)
+flaminggo = Hero("Flaminggo", 100, 35, 25)
+
+luffy.serang(flaminggo, luffy.power)
