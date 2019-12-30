7301d24ad0db68fc93ef04b4c5db0402f27dd579
Andrea Kurniawan <andreakurniawan98@gmail.com>
2019-12-30 21:55:45 +0700

    Melakukan impor modul, dan membuat object dari class

--- latihan/Hero.py
+++ latihan/Hero.py
@@ -1,10 +1,10 @@
 class Hero:
 
     def __init__(self, name):
-        self.__name = name
-        self.health__pool = [0, 100, 200, 300, 400, 500]
+        self.health_pool = [0, 100, 200, 300, 400, 500]
         self.attPower_pool = [0, 10, 20, 30, 40, 50]
         self.armor_pool = [0, 1, 2, 3, 4, 5]
+        self.__name = name
         self.__exp = 0
         self.__level = 0
 
@@ -41,7 +41,7 @@ class Hero:
 
     # Setter
     @health_pool.setter
-    def healt_pool(self, input):
+    def health_pool(self, input):
         self.__health_pool = input
 
     @attPower_pool.setter
@@ -56,7 +56,7 @@ class Hero:
     def gainExp(self, input):
         self.__exp += input
         if self.__exp >= 100:
-            self.__levelUp = self.__exp // 100
+            self.levelUp = self.__exp // 100
             self.__exp %= 100
 
     @levelUp.setter
@@ -71,7 +71,7 @@ class HeroIntelligent(Hero):
 
     def __init__(self, name):
         super().__init__(name)
-        self.healt_pool = [0, 50, 10, 150, 200, 250, ]
+        self.health_pool = [0, 50, 100, 150, 200, 250]
         self.attPower_pool = [0, 20, 40, 60, 80, 100]
         self.armor_pool = [0, 0.5, 1, 1.5, 2, 2.5]
         self.levelUp = 1
